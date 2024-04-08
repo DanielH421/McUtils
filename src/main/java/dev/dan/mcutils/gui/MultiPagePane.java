e808deecf70711eef601ccf9b61db3dd1533f86c
DanielH421 <daniel.hockridge@gmail.com>
2024-04-08 12:45:32 -0600

    I overcomplicated it.

--- src/main/java/dev/dan/mcutils/gui/MultiPagePane.java
+++ src/main/java/dev/dan/mcutils/gui/MultiPagePane.java
@@ -1,94 +1,69 @@
 package dev.dan.mcutils.gui;
 
 import com.github.stefvanschie.inventoryframework.gui.GuiItem;
+import com.github.stefvanschie.inventoryframework.pane.PaginatedPane;
 import com.github.stefvanschie.inventoryframework.pane.Pane;
-import com.github.stefvanschie.inventoryframework.pane.StaticPane;
 import com.github.stefvanschie.inventoryframework.pane.util.Slot;
 import dev.dan.mcutils.item.ItemCreator;
 import dev.dan.mcutils.utils.StringUtils;
 import org.bukkit.Material;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+public class MultiPagePane extends SimpleStaticPane {
 
-public class MultiPagePane extends StaticPane {
+    public PaginatedPane paginatedPane;
 
-    private HashMap<Integer, Pane> panes = new HashMap<>();
-
-    private List<SlotInfo> reservedSlots = new ArrayList<>();
-
-    private SlotInfo reservedBack = new SlotInfo(0, height);
-    private SlotInfo reservedNext = new SlotInfo(length, height);
+    private int currentPane;
 
     private GuiItem back = new GuiItem(new ItemCreator(Material.RED_STAINED_GLASS_PANE, StringUtils.altColor("&c&lBack")).getStack());
     private GuiItem next = new GuiItem(new ItemCreator(Material.GREEN_STAINED_GLASS_PANE, StringUtils.altColor("&e&lNext")).getStack());
 
     public MultiPagePane(Slot slot, int length, int height, @NotNull Pane.Priority priority) {
         super(slot, length, height, priority);
-        reserveSlots();
+        setup();
     }
 
     public MultiPagePane(int x, int y, int length, int height, @NotNull Pane.Priority priority) {
         super(x, y, length, height, priority);
-        reserveSlots();
+        setup();
     }
 
     public MultiPagePane(Slot slot, int length, int height) {
         super(slot, length, height);
-        reserveSlots();
+        setup();
     }
 
     public MultiPagePane(int x, int y, int length, int height) {
         super(x, y, length, height);
-        reserveSlots();
+        setup();
     }
 
     public MultiPagePane(int length, int height) {
         super(length, height);
-        reserveSlots();
-    }
-
-    
-    private void reserveSlots() {
-        reservedBack.setItem(back);
-        reservedNext.setItem(next);
-        reservedSlots.add(reservedNext);
-        reservedSlots.add(reservedBack);
+        setup();
     }
 
 
-    // TODO - Test
-    // This has not been tested.
-    public void setBackground(GuiItem item){
-        for (int x = 0; x < this.length; x++)   {
-            for (int y = 0; y < this.height; y++)   {
-                // This part is kind of gross. Sorry.
-                for(SlotInfo slotInfo : reservedSlots) {
-                    if(x != slotInfo.getX() && y != slotInfo.getY())
-                        this.addItem(item, x, y);
-                }
-            }
-        }
+    public void addSubPane(Pane pane) {
+        paginatedPane.addPage(pane);
     }
 
 
-    public void addBorder(GuiItem item) {
+    private void setup() {
+        paginatedPane = new PaginatedPane(length, height - 1);
+        this.addSubPane(paginatedPane);
 
-        // Also kinda gross
-        for (int x = 0; x < this.length; x++) {
-            this.addItem(item, x, 0);
-            for(SlotInfo slotInfo : reservedSlots) {
-                if(x != slotInfo.getX() && y != slotInfo.getY())
-                    this.addItem(item, x, this.height);
-            }
-        }
+        back.setAction(event -> {
+            currentPane++;
+            paginatedPane.setPage(currentPane);
+        });
+        next.setAction(event -> {
+            currentPane--;
+            paginatedPane.setPage(currentPane);
+        });
 
-        for (int y = 0; y < this.height; y++) {
-            this.addItem(item, 0, y);
-            this.addItem(item, 9 - 1, y);
-        }
+        this.addItem(back, 0, height);
+        this.addItem(next, length, height);
     }
-    
+
 }
