8d8706852d8cbe3b4010e9f45c3732bcbfeb5788
Dan <daniel.hockridge@gmail.com>
2024-04-08 14:47:14 -0600

    Finally make MultiPage stuff work.
    Next up.. Don't hard code the button's locations.

--- src/main/java/dev/dan/mcutils/gui/MultiPagePane.java
+++ src/main/java/dev/dan/mcutils/gui/MultiPagePane.java
@@ -1,6 +1,7 @@
 package dev.dan.mcutils.gui;
 
 import com.github.stefvanschie.inventoryframework.gui.GuiItem;
+import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
 import com.github.stefvanschie.inventoryframework.pane.PaginatedPane;
 import com.github.stefvanschie.inventoryframework.pane.Pane;
 import com.github.stefvanschie.inventoryframework.pane.util.Slot;
@@ -13,57 +14,94 @@ public class MultiPagePane extends SimpleStaticPane {
 
     public PaginatedPane paginatedPane;
 
-    private int currentPane;
+    private int currentPane = 0;
+
+    private ChestGui gui;
+
+    int maxIndex = -1;
 
     private GuiItem back = new GuiItem(new ItemCreator(Material.RED_STAINED_GLASS_PANE, StringUtils.altColor("&c&lBack")).getStack());
     private GuiItem next = new GuiItem(new ItemCreator(Material.GREEN_STAINED_GLASS_PANE, StringUtils.altColor("&e&lNext")).getStack());
 
-    public MultiPagePane(Slot slot, int length, int height, @NotNull Pane.Priority priority) {
+    public MultiPagePane(Slot slot, int length, int height, @NotNull Pane.Priority priority, ChestGui gui) {
         super(slot, length, height, priority);
-        setup();
+        this.gui = gui;
+        this.setup();
     }
 
-    public MultiPagePane(int x, int y, int length, int height, @NotNull Pane.Priority priority) {
+    public MultiPagePane(int x, int y, int length, int height, @NotNull Pane.Priority priority, ChestGui gui) {
         super(x, y, length, height, priority);
-        setup();
+        this.gui = gui;
+        this.setup();
     }
 
-    public MultiPagePane(Slot slot, int length, int height) {
+    public MultiPagePane(Slot slot, int length, int height, ChestGui gui) {
         super(slot, length, height);
-        setup();
+        this.gui = gui;
+        this.setup();
     }
 
-    public MultiPagePane(int x, int y, int length, int height) {
+    public MultiPagePane(int x, int y, int length, int height, ChestGui gui) {
         super(x, y, length, height);
-        setup();
+        this.gui = gui;
+        this.setup();
     }
 
-    public MultiPagePane(int length, int height) {
+    public MultiPagePane(int length, int height, ChestGui gui) {
         super(length, height);
-        setup();
+        this.gui = gui;
+        this.setup();
     }
 
 
-    public void addSubPane(Pane pane) {
+    public void addPage(Pane pane) {
         paginatedPane.addPage(pane);
+        maxIndex++;
+        if(paginatedPane.getPages() == 1) {
+            paginatedPane.setPage(currentPane);
+            gui.update();
+        }
     }
 
 
     private void setup() {
-        paginatedPane = new PaginatedPane(length, height - 1);
-        this.addSubPane(paginatedPane);
+        paginatedPane = new PaginatedPane(0, 0, this.length, this.height - 1);
+        gui.addPane(paginatedPane);
 
         back.setAction(event -> {
-            currentPane++;
+            event.setCancelled(true);
+            if(currentPane > 0)
+                currentPane--;
             paginatedPane.setPage(currentPane);
-        });
-        next.setAction(event -> {
-            currentPane--;
-            paginatedPane.setPage(currentPane);
+            gui.update();
         });
 
-        this.addItem(back, 0, height);
-        this.addItem(next, length, height);
+        next.setAction(event -> {
+            event.setCancelled(true);
+            if(currentPane < maxIndex)
+                currentPane++;
+            paginatedPane.setPage(currentPane);
+            gui.update();
+        });
+        addItem(back, 0, height-1);
+        addItem(next, length-1, height-1);
+        gui.update();
+    }
+
+
+    @Override
+    public void setBackground(GuiItem item){
+        super.setBackground(item);
+        addItem(back, 0, height-1);
+        addItem(next, length-1, height-1);
+    }
+
+
+    @Override
+    public void addBorder(GuiItem item) {
+        super.addBorder(item);
+        addItem(back, 0, height-1);
+        addItem(next, length-1, height-1);
     }
 
 }
